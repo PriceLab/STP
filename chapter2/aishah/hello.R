e2e67497d789a0eee47180d01b537636f64d5f3c
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-07-02 12:55:03 -0700

    hello, world

---
+++ chapter2/aishah/hello.R
@@ -0,0 +1,166 @@
+Aishah.~> cd
+Aishah.~> ls
+#.bash_profile#	#.bashrc#	Applications	Desktop		Documents	Downloads	Library		Movies		Music		Pictures	Public		anaconda3	github		isb projects	practice
+Aishah.~> cd github/
+Aishah.github> ls
+RCyjs			interactionNetworks	interactionNetworksOLD	project
+Aishah.github> git clone https://github.com/PriceLab/STP.git
+Cloning into 'STP'...
+remote: Counting objects: 15, done.        
+remote: Compressing objects: 100% (12/12), done.        
+remote: Total 15 (delta 2), reused 11 (delta 2), pack-reused 0        
+Unpacking objects: 100% (15/15), done.
+Aishah.github> ls
+RCyjs			STP			interactionNetworks	interactionNetworksOLD	project
+Aishah.github> cd STP
+Aishah.STP> ls
+LICENSE		README.md	chapter2
+Aishah.STP> cd chapter2/aishah/
+Aishah.aishah> ls
+hello.R		hello.py
+Aishah.aishah> R
+
+R version 3.5.0 (2018-04-23) -- "Joy in Playing"
+Copyright (C) 2018 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin15.6.0 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+Error: attempt to use zero-length variable name
+> source("hello.R")
+> run()
+Error in run() : could not find function "run"
+> q()
+Aishah.aishah> ls
+hello.R		hello.py
+Aishah.aishah> R
+
+R version 3.5.0 (2018-04-23) -- "Joy in Playing"
+Copyright (C) 2018 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin15.6.0 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+Error: attempt to use zero-length variable name
+> source("hello.R")
+> 
+> 
+> print("hello, world")
+[1] "hello, world"
+> q()
+Aishah.aishah> R
+
+R version 3.5.0 (2018-04-23) -- "Joy in Playing"
+Copyright (C) 2018 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin15.6.0 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+Error: attempt to use zero-length variable name
+> source("hello.R")
+Error in source("hello.R") : hello.R:1:9: unexpected '>'
+1: Aishah.~>
+            ^
+> q()
+Aishah.aishah> 
+Aishah.aishah> 
+Aishah.aishah> 
+Aishah.aishah> 
+Aishah.aishah> 
+Aishah.aishah> 
+Aishah.aishah> python
+Python 2.7.10 (default, Oct  6 2017, 22:29:07) 
+[GCC 4.2.1 Compatible Apple LLVM 9.0.0 (clang-900.0.31)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> q(0
+q(0
+... q()
+q()
+  File "<stdin>", line 2
+    q()
+    ^
+SyntaxError: invalid syntax
+>>> end
+end
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'end' is not defined
+>>> print("hello, world")
+print("hello, world")
+hello, world
+>>> pwd
+pwd
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'pwd' is not defined
+>>> 
+Aishah.aishah> ls
+ls
+hello.R		hello.R~	hello.py
+Aishah.aishah> R
+R
+
+R version 3.5.0 (2018-04-23) -- "Joy in Playing"
+Copyright (C) 2018 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin15.6.0 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+Error: attempt to use zero-length variable name
+> source("hello.R")
+source("hello.R")
+Error in source("hello.R") : hello.R:1:9: unexpected '>'
+1: Aishah.~>
+            ^
+> print("hello, world")
+print("hello, world")
+[1] "hello, world"
+> 
