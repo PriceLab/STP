59aec415f499b52ad3c4f67c77f3e3417cde9900
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-07-03 15:43:47 -0700

    translated in R

---
+++ chapter2/aishah/challenges.R
@@ -0,0 +1,42 @@
+//1
+
+ISB = c("aishah" , "omar" , "paul")
+for (i in ISB){
+    print(paste(i))
+}
+
+
+
+//2
+
+x = 10
+if (x < 10) {
+    print("less than 10")
+} else {
+    print("equal or greater than 10")
+}
+
+
+//3
+
+y = 10
+if (y <= 10) {
+    print("less than or equal to 10")
+} else if(y > 10){
+    print("greater than 10")
+} else if (y > 25){
+    print("greater than 25")
+    }
+
+
+//4
+g= 3
+h= 7
+h %% g
+
+
+//5
+
+26%/% 8
+
+//6
