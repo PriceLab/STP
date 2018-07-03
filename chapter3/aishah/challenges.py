212e8855f2b49839ce849dec97f8c6134bd90cc3
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-07-03 16:50:59 -0700

    explained modules

--- chapter3/aishah/challenges.py
+++ chapter3/aishah/challenges.py
@@ -1,18 +1,20 @@
-# 1
+# 1 ------ print 3 strings
 
 ISB = ["aishah", "omar", "paul"]
 for n in ISB:
     print(n)
 
 
-# 2
+# 2 ----- print a message for a variable less than 10 and a different one for
+#       a varriable being greater than 10
 x = 10
 if x < 10:
     print ("x is less than 10")
 elif x >= 10:
     print ("x is greater or equal to 10")
 
-# 3
+# 3 ----- print message if variable less than or equal to 10, if its greater than ten
+#         but less than 25
 y = 10
 if y <=  10:
     print ("y is less than or equal to 10")
@@ -22,18 +24,18 @@ else:
     print("y is greater than 25")
 
 
-# 4: demonstrate the modulo operator (what does
+#4 ------ division operatorn 
 14 % 4
 
 
-# 5
-x = 5
-y = 8
+# 5 ------ create a program that takes two variables, divides, and prints quotient
+x = 100
+y = 4
 x // y
 
 
 
-#6
+#6 ----- assign 'age' to a number and create a program that prints different strings depending on what integer 'age' is
 age = 16
 adult = age + 5
 
