179e91770e2d34d80ec95d6004a6801ff3dd3c4f
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-07-03 15:23:35 -0700

    new files

---
+++ chapter2/aishah/challenges.py
@@ -0,0 +1,33 @@
+#1
+
+ISB = ["aishah", "omar", "paul"]
+for n in ISB:
+    print(n)
+
+
+#2
+x = 10
+if x < 10:
+    print ("x is less than 10")
+elif x >= 10:
+    print ("x is greater or equal to 10")
+
+#3
+y = 10
+if y <=  10:
+    print ("y is less than or equal to 10")
+elif y > 10 and y<= 25:
+    print ("y is between 10 and 25")
+else:
+    print("y is greater than 25")
+
+
+#4
+14 % 4
+
+
+#5
+x = 5
+y = 8
+x \\ y
+
