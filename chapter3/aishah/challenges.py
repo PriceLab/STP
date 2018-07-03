c010148d49f0d4d67dc76c783efe3a9afc919cc8
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-07-03 16:35:56 -0700

    added a comment or two

--- chapter3/aishah/challenges.py
+++ chapter3/aishah/challenges.py
@@ -1,18 +1,18 @@
-#1
+# 1
 
 ISB = ["aishah", "omar", "paul"]
 for n in ISB:
     print(n)
 
 
-#2
+# 2
 x = 10
 if x < 10:
     print ("x is less than 10")
 elif x >= 10:
     print ("x is greater or equal to 10")
 
-#3
+# 3
 y = 10
 if y <=  10:
     print ("y is less than or equal to 10")
@@ -22,14 +22,14 @@ else:
     print("y is greater than 25")
 
 
-#4
+# 4: demonstrate the modulo operator (what does
 14 % 4
 
 
-#5
+# 5
 x = 5
 y = 8
-x \\ y
+x // y
 
 
 
