5c5fa25c2bd2bdc9702583a3f0ad5061424eb64d
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-07-05 10:21:11 -0700

    fixed R code

--- chapter3/aishah/challenges.R
+++ chapter3/aishah/challenges.R
@@ -1,18 +1,18 @@
-//1 ------ print 3 strings
+#1 ------ print 3 strings
 
 
-ISB = c("aishah" , "omar" , "paul")
-for (i in ISB){
-    print(paste(i))
+ISB <- c("aishah" , "omar" , "paul")
+for (employee in ISB){
+    print(employee)
 }
 
 
 
-//2 ----- print a message for a variable less than 10 and a different one for
-//       a varriable being greater than 10
+#2 ----- print a message for a variable less than 10 and a different one for
+#       a varriable being greater than 10
 
 
-x = 10
+x <- 10
 if (x < 10) {
     print("less than 10")
 } else {
@@ -20,11 +20,11 @@ if (x < 10) {
 }
 
 
-//3 ----- print message if variable less than or equal to 10, if its greater than ten
-//         but less than 25
+#3 ----- print message if variable less than or equal to 10, if its greater than ten
+#         but less than 25
 
 
-y = 10
+y <- 10
 if (y <= 10) {
     print("less than or equal to 10")
 } else if(y > 10){
@@ -34,22 +34,22 @@ if (y <= 10) {
     }
 
 
-//4 ------ division operatorn
+#4 ------ division operatorn
 
-g= 3
-h= 7
+g <- 3
+h <- 7
 h %% g
 
 
-// 5 ------ create a program that takes two variables, divides, and prints quotient
+#5 ------ create a program that takes two variables, divides, and prints quotient
 
 
 26%/% 8
 
-//6 ----- assign 'age' to a number and create a program that prints different strings depending on what integer 'age' is
+#6 ----- assign 'age' to a number and create a program that prints different strings depending on what integer 'age' is
 
-age = 16
-adult = age + 5
+age <- 16
+adult <- age + 5
 
 if (age < adult){
     print("you're a teenager.")
