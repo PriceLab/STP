568c5ec0ada978bef815dd6d627e7e52d0c9ebda
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-07-06 10:18:36 -0700

    ch5 challenges

--- chapter5/aishah/challenges.R
+++ chapter5/aishah/challenges.R
@@ -2,17 +2,14 @@
 musicians <- c('drake', 'PnB rock', 'a boogie')
 
 #2---- create a dictionary of your attributes: height, age, favourite colour
-facts <- dict()
+#facts["height"] <- "5'2"
+#facts["favourite colour"] <- "red"
+#facts["age"] <- "19"
 
-facts["weight"] <- "5'2"
-facts["favourite colour"] <- "red"
-facts["age"] <- "19"
+facts <- c("height": "5'2", "favourite colour":"red", "age":"19")
 
-facts["height"]
-facts["age"]
-facts["favourite colour"]
 
-#3----create a directory mapping your favourite artists to a list of your favourite songs by them
+#3----create a directory mapping your favourite artists to a lisnt of your favourite songs by them
 
 musicians <- c('drake', 'PnB rock', 'a boogie')
-songs <- c("drake": "come thru", "PnB rock":"lovin u", "a boogie":"jungle)
+songs <- c("drake": "come thru", "PnB rock":"lovin u", "a boogie":"jungle")
