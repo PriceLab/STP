7dc747889f9a005f601acbc55419df3ec047cfc8
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-08-28 11:13:59 -0700

    multiple tests PASS. fixed (sample) code

--- graphs/igraphToJSON/test_utils.R
+++ graphs/igraphToJSON/test_utils.R
@@ -10,46 +10,52 @@ runTests <- function()
    test_1NodeTestGraph()
    test_15NodeTestGraph()
    test_4Node2EdgesTestGraph()
+   test_5Node2EdgesTestGraph_withAttr()
 
 } # runTests
 #----------------------------------------------------------------------------------------------------
 test_createEmptyTestGraph <- function()
 {
-
+   printf("---------------test_createEmptyTestGraph")
    g <- createTestGraph()        # an empty graph, always a good place to start
    checkEquals(length(V(g)), 0)
-    checkEquals(length(E(g)), 0)
-    printf("------- test Empty graph")
+   checkEquals(length(E(g)), 0)
+
 
 } # test_createGraphs
 #----------------------------------------------------------------------------------------------------
 test_1NodeTestGraph <- function()
 {
-
-   g <- createTestGraph(1)        # 1 node graph
+    printf("---------------test_1NodeTestGraphh")
+    g <- createTestGraph(1)        # 1 node graph
     checkEquals(length(V(g)), 1)
     checkEquals(length(E(g)), 0)
-    printf("------- test 1 node graph")
 
 } # test_createGraphs
 #----------------------------------------------------------------------------------------------------
 test_15NodeTestGraph <- function()
 {
-
-   g <- createTestGraph(15)        # 15 node graph
+    printf("---------------test_15NodeTestGraph")
+    g <- createTestGraph(15)        # 15 node graph
     checkEquals(length(V(g)), 15)
     checkEquals(length(E(g)), 0)
-     printf("------- test 15 node graph")
 
 } # test_createGraphs
 #----------------------------------------------------------------------------------------------------
 test_4Node2EdgesTestGraph <- function()
 {
-
-    g <- createTestGraph(4,2)        # 4 node graph
+    printf("---------------4Node2EdgesTestGraph")
+    g <- createTestGraph(4,2)        # 4 node 2 edge graph
     checkEquals(length(V(g)), 4)
     checkEquals(length(E(g)), 2)
-     printf("------- test 4 node, 2 edge graph")
+} # test_createGraphs
+#----------------------------------------------------------------------------------------------------
+test_5Node2EdgesTestGraph_withAttr<- function()
+{
+    printf("---------------5Node2EdgesTestGraph_withAttr")
+    g <- createTestGraph(5,2, c("red", "white", "blue", "yellow", "black"), c("one", "two"))
+    checkEquals(length(V(g)), 5)              # 5 nodes, 2edge graph. with attributes
+    checkEquals(length(E(g)), 2)
 } # test_createGraphs
 #----------------------------------------------------------------------------------------------------
 
