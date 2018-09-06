5617492bae6f87dcbc6db747315a355520255704
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-09-06 14:43:19 -0700

    tests

--- graphs/igraphToJSON/exploration.R
+++ graphs/igraphToJSON/exploration.R
@@ -14,9 +14,16 @@ timesTen <- function(list)
 
 } # timesTen
 #----------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+    test_timesTen()
+    test_timesTen_lst2()
+    test_timesTen_lst3()
+}
+#----------------------------------------------------------------------------------------------------
 test_timesTen <- function()
 {
-    printf("--- test_timesTen")
+    print("--- test_timesTen")
 
     expected.results <- c(10,20,30,40,50)
     result <- timesTen(1:5)
@@ -24,4 +31,26 @@ test_timesTen <- function()
     checkEquals(result, expected.results)
 
 } # test_timesTen
+#----------------------------------------------------------------------------------------------------
+test_timesTen_lst2<- function()
+{
+    print("--- test_timesTen_lst2")
+
+    expected.results <- c(100,70,80,2000,50)
+    result <- timesTen(c(10, 7, 8, 200, 5))
+
+    checkEquals(result, expected.results)
+
+} # test_timesTen_lst2
+#----------------------------------------------------------------------------------------------------
+test_timesTen_lst3<- function()
+{
+    print("--- test_timesTen_lst3")
+
+    expected.results <- c(0)
+    result <- timesTen(0)
+
+    checkEquals(result, expected.results)
+
+} # test_timesTen_lst3
 #----------------------------------------------------------------------------------------------------
