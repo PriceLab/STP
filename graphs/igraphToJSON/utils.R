32005fd3e2dbe252b2edda68aa1b3ca059b40e22
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-09-26 12:04:55 -0700

    multiple node and edge attr tests

--- graphs/igraphToJSON/utils.R
+++ graphs/igraphToJSON/utils.R
@@ -1,16 +1,5 @@
 # utils.R:  utilitiy functions using igraph
 #------------------------------------------------------------------------------------------------------------------------
-# retun an igraph instance with the specified number of nodes, edges and attributes
-# name nodes in the style "node1", "node2", ...
-# name edges similarly: "edge1", "edge2"
-# create edges by randomly choosing pairs of nodes to connect:
-#   nodes <- paste("node", 1:5, sep="")
-#   nodePairs <- nodes[sample(length(nodes), 2)]
-#   nodeAttribues and edgeAttributes will each be a simple list of named attributes to create,
-#     each with a random value, for instance
-#          edge.attribute.1 <- 100 * runif(1)   # a numeric attribute
-                                        #          edge.attribute.2 <- LETTERS[sample(1:26, 1)]
-#------------------------------------------------------------------------------------------------------------------------
 library(igraph)
 #----------------------------------------------------------------------------------------------------
 makeRandomString <- function(list)
