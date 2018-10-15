08ec1e9be97629d839aa8a71af3d1918463b64e5
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-10-15 12:14:53 -0700

    practice test

--- graphs/igraphToJSON/addNodeAttributesToGraph.R
+++ graphs/igraphToJSON/addNodeAttributesToGraph.R
@@ -1,27 +1,38 @@
 # given an igraph with zero or more nodes, add a random integer attribute, of the specified name
 # to each node in the graph
+library(igraph)
+library(RUnit)
 #------------------------------------------------------------------------------------------------------------------------
-addNodeAttributesToGraph(igraph, attributeName, nodeNames, noaValues)
+addNodeAttributesToGraph <- function(igraph, attributeName, nodeNames, noaValues)
 {
-   # all nodeNames actually the names of node in the igraph?
-   # length (nodeNames) == length(noaValues)?
+
+    graph.out <- set.vertex.attribute(igraph, attributeName, nodeNames, noaValues)
+
+    return(graph.out)
 
 } # addNodeAttributesToGraph
 #------------------------------------------------------------------------------------------------------------------------
-test_addNodeAttributesToGraph(igraph, attributeName, nodeNames, noaValues)
+test_addNodeAttributesToGraph <- function(igraph, attributeName, nodeNames, noaValues)
 {
    printf("--- test_addNodeAttributesToGraph")
 
-     # make a 5 node, 0 edge graph, no node attributes
+   # make a 5 node, 0 edge graph, no node attributes
    node.names <- LETTERS[1:5]
    g <- graph_from_literal() + vertices(node.names)
 
    sizes <- c(10, 20, 30, 40, 50)
-   g.noa <- addNodeAttributesToGraph(g, "size", node.names, sizes)
+   g.noa <-addNodeAttributesToGraph(g, "size", node.names, sizes)
 
-      # tests
-   # does the nade attribute "size" truly exist on the graph's nodes?
-   # does node A have size 10?  B has 20?  C 30, D 40, E 50?
+   for(i in V(g.noa))
+   {
+       checkTrue(get.vertex.attribute(g.noa)$name[i] == node.names[i], get.vertex.attribute(g.noa)$size[i] == sizes[i])
+        } #for i
+
+   noa.size <- get.vertex.attribute(g.noa, "size")
+   checkEquals(length(noa.size),5)
+
+   noa.letters <- get.vertex.attribute(g.noa, "name")
+   checkEquals(length(noa.letters),5)
 
 } # test_addNodeAttributesToGraph
 #------------------------------------------------------------------------------------------------------------------------
