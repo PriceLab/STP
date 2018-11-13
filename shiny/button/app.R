a3d822a40fbbfb3c845f15f270a2a4d534c85b46
aishahmohamed98 <aishah.mohamed98@gmail.com>
2018-11-13 13:44:51 -0800

    shiny app.R

--- shiny/button/app.R
+++ shiny/button/app.R
@@ -1,47 +1,64 @@
 library(shiny)
-source("smalldata.txt")
-tbl <- read.table("smalldata.txt", sep = "\t",
-                  header = TRUE,
-                  stringsAsFactors=FALSE)
+fileNames <- c("smalldata.txt", "smalldata2.txt", "smalldata3.txt")
 #----------------------------------------------------------------------------------------------------
 if (interactive()) {
-ui <-pageWithSidebar(
+ui <- pageWithSidebar(
     headerPanel("Plot"),
     sidebarPanel(
-        selectInput("colOption1", #drop down menu 1
-                    label = "Choose a X variable to display",
-                    choices = c(colnames(tbl))),
-                   selected = "option",
-         textOutput("selected_colOption1"),
+                selectInput("selectFileMenu",
+                label = "Selected file", #first check box
+                choices= c(fileNames)),
 
-         selectInput("colOption2", #drop down menu 2
-                   label = "Choose a Y variable to display",
-                   choices = c(colnames(tbl))),
-                   selected = "option",
-        textOutput("selected_colOption2")
-        ),
-    mainPanel(
+    selectInput("colOption1",
+                label = "Select X",
+                choices = c(colnames(df))),
+
+
+    selectInput("colOption2",
+                label = "select Y",
+                choices = c(colnames(df)))
+    ),
+        mainPanel(
         fluidPage(
             actionButton("goButton", "Go!"),
             plotOutput("xyPlot"),
             verbatimTextOutput("info"))
             )
-)
+) # ui
 #----------------------------------------------------------------------------------------------------
-server <- function(input, output) {
+server <- function(input, output, session) {
+         output$xyPlot <- renderPlot({
+             plot(0, col="white")
+             }) # empty plot
 
-    output$selected_var <- renderText({
-        paste("X value:",  input$colOption1, "Y value:", input$colOption2)
-    })
+         observe({
+             selectedFile <- input$selectFileMenu
+             print(selectedFile)
+             df <- read.table(selectedFile, header = TRUE, sep = "\t", stringsAsFactors= FALSE)
 
-    output$xyPlot <- renderPlot({
-        input$goButton
-        colOption1 = isolate(input$colOption1)
-        colOption2 = isolate(input$colOption2)
-        plot(tbl[, colOption1], tbl[,colOption2])
-        })
+             updateSelectInput(session,"colOption1",
+                               label = paste("Select X"),
+                               choices = colnames(df))
 
-}
+             updateSelectInput(session, "colOption2",
+                                     label = paste("Select label Y"),
+                                     choices = colnames(df))
+
+         observeEvent(input$goButton,{ # go button to plot
+             topMenuChoice <- isolate(input$colOption1)
+             bottomMenuChoice <- isolate(input$colOption2)
+
+             output$xyPlot <- renderPlot({
+                 plot(df[,topMenuChoice], df[,bottomMenuChoice], main="Plot Hypothesis",
+                      xlab= topMenuChoice, ylab= bottomMenuChoice)
+                 model <- lm(df[, topMenuChoice] ~ df[, bottomMenuChoice])
+                 abline(model)
+                 print(summary(model))
+             })
+
+         }) # observeEvent
+             }) # observe
+} # server
 #----------------------------------------------------------------------------------------------------
 app <- shinyApp(ui, server)
 shinyApp(ui = ui, server = server)
